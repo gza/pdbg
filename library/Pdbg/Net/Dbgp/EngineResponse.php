75dea65282b84a6a342b8b0e4eb1aa141a8d3549
baron314159 <baron314159@yahoo.com>
2008-06-20 02:17:57 +0000

    Further developed engine response code.

--- library/Pdbg/Net/Dbgp/EngineResponse.php
+++ library/Pdbg/Net/Dbgp/EngineResponse.php
@@ -26,6 +26,11 @@
  */
 
 /**
+ * @see Pdbg_Net_Dbgp_EngineResponse_Exception
+ */
+require_once 'Pdbg/Net/Dbgp/EngineResponse/Exception.php';
+
+/**
  * Represents a debugger engine response.
  *
  * @category   Development
@@ -43,20 +48,115 @@ class Pdbg_Net_Dbgp_EngineResponse
      */
     protected $_doc;
 
+    /**
+     * Constructs an instance.
+     *
+     * @param string|DOMDocument $xmlData
+     * @return void
+     */
     public function __construct($xmlData)
     {
-        $this->_doc = new DOMDocument();
-        $this->_doc->loadXML($xmlData);
+        if ($xmlData instanceof DOMDocument) {
+            $this->_doc = $xmlData;
+        } else {
+            $this->_doc = new DOMDocument();
+            $this->_doc->loadXML($xmlData);
+        }
+
         $this->_doc->formatOutput = true;
     }
 
+    /**
+     * Returns a DOMDocument containing the response XML.
+     *
+     * @return DOMDocument
+     */
     public function getDocument()
     {
         return $this->_doc;
     }
 
+    /**
+     * Returns the raw XML of the response.
+     *
+     * @return string
+     */
     public function getXml()
     {
         return $this->_doc->saveXML();
+    }
+
+    /**
+     * Returns the transaction id of the command causing this response.
+     *
+     * @return integer
+     * @throws Pdbg_Net_Dbgp_EngineResponse_Exception
+     */
+    public function getTransactionId()
+    {
+        $root = $this->_doc->documentElement;
+
+        if (!$root->hasAttribute('transaction_id')) {
+            throw new Pdbg_Net_Dbgp_EngineResponse_Exception("transaction_id attribute does not exist");
+        } else {
+            return intval($root->getAttribute('transaction_id'));
+        }
+    }
+
+    /**
+     * Returns the command causing this engine response.
+     *
+     * @return string
+     */
+    public function getCommand()
+    {
+        return self::getCommandFromDocument($this->_doc);
+    }
+
+    /**
+     * Reads the command from a DOMDocument.
+     *
+     * @param DOMDocument $doc
+     * @return string
+     * @throws Pdbg_Net_Dbgp_EngineResponse_Exception
+     */
+    public static function getCommandFromDocument(DOMDocument $doc)
+    {
+        $root = $doc->documentElement;
+
+        if (!$root->hasAttribute('command')) {
+            throw new Pdbg_Net_Dbgp_EngineResponse_Exception("command attribute does not exist");
+        } else {
+            return $root->getAttribute('command');
+        }
+    }
+
+    /**
+     * Returns true if the ide command causing this response was executed
+     * successfully be the debugger engine.
+     *
+     * @return boolean
+     */
+    public function commandSuccessful()
+    {
+        return self::commandSuccessfulFromDocument($this->_doc);
+    }
+
+    /**
+     * Returns true if the ide command causing the response in the supplied
+     * DOMDocument was executed successfully be the debugger engine.
+     *
+     * @param DOMDocument $doc
+     * @return boolean
+     */
+    public static function commandSuccessfulFromDocument(DOMDocument $doc)
+    {
+        $root = $doc->documentElement;
+
+        if (!$root->hasAttribute('success')) {
+            return false;
+        } else {
+            return ($root->getAttribute('success') == '1');
+        }
     }
 }
