7fd39b16f57462e7d685e3d3af11ccd3ae8ea70f
baron314159@yahoo.com <baron314159@yahoo.com@d809c3fb-124f-0410-adc8-2fa0b196bfb9>
2008-07-13 19:26:35 +0000

    Development of toolbar manager.

--- branches/python-rewrite/pdbg/gtk/mgr/toolbar.py
+++ branches/python-rewrite/pdbg/gtk/mgr/toolbar.py
@@ -8,6 +8,10 @@ __version__ = "$Id$"
 from ...app.patterns import Manager, Singleton
 from ..view.app import AppView
 
+_continuation_commands = (
+    'run', 'detach', 'step_into', 'step_over', 'step_out'
+)
+
 class ToolbarManager(Manager, Singleton):
 
     """Management of the toolbar ui."""
@@ -18,4 +22,10 @@ class ToolbarManager(Manager, Singleton):
 
     def setup(self):
         """Setup the manager."""
+        app_view = AppView.get_instance()
+        for command in _continuation_commands:
+            widget = app_view['toolbar_' + command]
+            widget.connect('clicked', self.on_continuation_clicked, command)
+
+    def on_continuation_clicked(self, button, command_name):
         pass
