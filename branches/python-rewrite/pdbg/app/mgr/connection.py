1217e52d6d59305fcc6a5f561432c2afeeec2766
baron314159@yahoo.com <baron314159@yahoo.com@d809c3fb-124f-0410-adc8-2fa0b196bfb9>
2008-07-14 00:37:02 +0000

    Development of the toolbar continuation commands.

--- branches/python-rewrite/pdbg/app/mgr/connection.py
+++ branches/python-rewrite/pdbg/app/mgr/connection.py
@@ -5,19 +5,21 @@
 
 __version__ = "$Id$"
 
+import re
 from pdbg.app.patterns import Manager, Singleton
 from pdbg.dbgp.socketwrapper import SocketWrapper
 from pdbg.dbgp.connectionlistener import ConnectionListener
 from pdbg.dbgp.idecommand import *
 from pdbg.dbgp.engineresponse import *
 
-_event_names = [
+_event_names = (
+    'state_changed',
     'command_sent',
     'response_received',
     'init_packet',
     'init_source',
     'init_status'
-]
+)
 
 def expected_response(type):
     def decorator(fn):
@@ -28,56 +30,103 @@ def expected_response(type):
         return check_type
     return decorator
 
-class AwaitingInitState(object):
+class ConnectionState(object):
+
+    @classmethod
+    def get_name(klass):
+        underscore = lambda m: '_' + m.group(0).lower()
+        return re.sub('[A-Z]', underscore, klass.__name__).lstrip('_')
+
+class AwaitingInit(ConnectionState):
 
     @classmethod
     @expected_response(InitResponse)
     def run(klass, mgr, init):
         remote_address = mgr.connection.socket.handle.getpeername()
         mgr.fire('init_packet', init, remote_address)
-        mgr.send_command('source', { '-f': init.file_uri })
-        return AwaitingInitSourceState
+        mgr.send_command_stateless('source', { '-f': init.file_uri })
+        return AwaitingInitSource
 
-class AwaitingInitSourceState(object):
+class AwaitingInitSource(ConnectionState):
 
     @classmethod
     @expected_response(SourceResponse)
     def run(klass, mgr, response):
         mgr.fire('init_source', response)
-        mgr.send_command('status')
-        return AwaitingInitStatusState
+        mgr.send_command_stateless('status')
+        return AwaitingInitStatus
 
-class AwaitingInitStatusState(object):
+class AwaitingInitStatus(ConnectionState):
 
     @classmethod
     @expected_response(StatusResponse)
     def run(klass, mgr, response):
         mgr.fire('init_status', response)
-        return CanInteractState
+        return CanInteract
 
-class CanInteractState(object):
+class CanInteract(ConnectionState):
 
     @classmethod
     def run(klass, mgr, response):
         return None
 
+class CannotInteract(ConnectionState):
+
+    @classmethod
+    def run(klass, mgr, response):
+        return CanInteract
+
+class ConnectionManagerException(Exception):
+    pass
+
 class ConnectionManager(Manager):
 
     def __init__(self):
         super(Manager, self).__init__()
-        self._state = AwaitingInitState
+        self._state = AwaitingInit
         self.register_event(*_event_names)
+
+    def _change_state(self, state):
+        self.fire('state_changed', self._state.get_name(), state.get_name())
+        self._state = state
 
     @property
     def connection(self):
         return self._connection
 
+    @property
+    def can_interact(self):
+        return self._state == CanInteract
+
     def setup(self, connection):
         self._connection = connection
 
-    def send_command(self, command_str, arguments=[], data=None):
+    def send_command(self, command_str, arguments=[], data=None, \
+        req_state=CanInteract, change_state=CannotInteract):
+        if req_state != None and self._state != req_state:
+            raise ConnectionManagerException, "cannot send command in current state"
+        self.send_command_stateless(command_str, arguments, data)
+        if change_state != None:
+            self._change_state(change_state)
+
+    def send_command_stateless(self, command_str, arguments=[], data=None):
         command = self.connection.send_command(command_str, arguments, data)
         self.fire('command_sent', command)
+
+    def send_run(self):
+        self.send_command('run')
+
+    def send_detach(self):
+        self.send_command('detach')
+
+    def send_step_into(self):
+        self.send_command('step_into')
+
+    def send_step_over(self):
+        self.send_command('step_over')
+
+    def send_step_out(self):
+        self.send_command('step_out')
 
     def process_response(self):
         response = self._connection.recv_response()
@@ -85,4 +134,4 @@ class ConnectionManager(Manager):
             self.fire('response_received', response)
             new_state = self._state.run(self, response)
             if new_state != None:
-                self._state = new_state
+                self._change_state(new_state)
--- branches/python-rewrite/pdbg/gtk/mgr/pages.py
+++ branches/python-rewrite/pdbg/gtk/mgr/pages.py
@@ -10,6 +10,11 @@ from ...app.mgr.listener import ListenerManager
 from page import PageManager
 from ..view.app import AppView
 
+_event_names = (
+    'page_mgr_deactivated',
+    'page_mgr_activated'
+)
+
 class PagesManager(Manager, Singleton):
 
     """Management of ui tab pages.
@@ -21,12 +26,26 @@ class PagesManager(Manager, Singleton):
 
     def __init__(self):
         """Construct an instance."""
+        super(PagesManager, self).__init__()
         self._page_mgrs = []
+        self._active_mgr = None
+        self.register_event(*_event_names)
 
     def setup(self):
         """Setup the manager."""
         listener_mgr = ListenerManager.get_instance()
         listener_mgr.add_observer('new_connection', self.on_new_connection)
+
+        app_view = AppView.get_instance()
+        app_view['notebook'].connect('switch-page', self.on_switch_page)
+
+    def on_switch_page(self, notebook, page, page_num):
+        if self._active_mgr != None:
+            self.fire('page_mgr_deactivated', self._active_mgr)
+            self._active_mgr = None
+        if page_num > 0:
+            self._active_mgr = self._page_mgrs[page_num-1]
+            self.fire('page_mgr_activated', self._active_mgr)
 
     def on_new_connection(self, mgr, connection):
         """Handle the establishment of new DBGp connections.
