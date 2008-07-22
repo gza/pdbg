4db59d4d016b3b8697d1b971285a7e5e16c2bec5
baron314159 <baron314159@yahoo.com>
2008-07-22 02:02:08 +0000

    Changed sourceview widget to gtksourceview2.

--- pdbg/gtk/widget/sourceview.py
+++ pdbg/gtk/widget/sourceview.py
@@ -6,24 +6,34 @@
 __version__ = "$Id$"
 
 import gtk
-import gtksourceview as sv
+import gtksourceview2 as gtksourceview
 import pango
 import time
 import re
 
-class SourceView(sv.SourceView):
+def _get_language_for_mime_type(mime):
+    lang_manager = gtksourceview.language_manager_get_default()
+    lang_ids = lang_manager.get_language_ids()
+    for i in lang_ids:
+        lang = lang_manager.get_language(i)
+        for m in lang.get_mime_types():
+            if m == mime:
+                return lang
+    return None
+
+class SourceView(gtksourceview.View):
 
     def __init__(self):
-        sv.SourceView.__init__(self)
+        gtksourceview.View.__init__(self)
 
         self._current_line = None
 
-        lang_mgr = sv.SourceLanguagesManager()
-        php_lang = lang_mgr.get_language_from_mime_type('application/x-php')
+        lang_mgr = gtksourceview.LanguageManager()
+        php_lang = _get_language_for_mime_type('application/x-php')
 
-        buffer = sv.SourceBuffer()
+        buffer = gtksourceview.Buffer()
         buffer.set_language(php_lang)
-        buffer.set_highlight(True)
+        buffer.set_highlight_syntax(True)
 
         self.set_show_line_numbers(True)
         self.set_editable(False)
@@ -45,6 +55,7 @@ class SourceView(sv.SourceView):
             buffer.remove_tag_by_name('current_line', iter1, iter2)
         self._current_line = None
 
+
     def _get_line_iters(self, line_num):
         buffer = self.get_buffer()
         iter1 = buffer.get_iter_at_line(line_num)
@@ -53,7 +64,7 @@ class SourceView(sv.SourceView):
         return (iter1, iter2)
 
     def _add_tags(self):
-        tag = gtk.TextTag("current_line")
+        tag = gtk.TextTag('current_line')
         tag.set_property('foreground', '#ffffff')
         tag.set_property('foreground-set', True)
         tag.set_property('paragraph-background', '#444466')
