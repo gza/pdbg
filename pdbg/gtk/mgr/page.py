a7244fe3a01c5461fc6f3dad0233fe312906e128
baron314159 <baron314159@yahoo.com>
2008-08-02 12:48:16 +0000

    Commented on_* methods in the PageManager class; sourceview is not updated if source is already set.

--- pdbg/gtk/mgr/page.py
+++ pdbg/gtk/mgr/page.py
@@ -77,6 +77,8 @@ class PageManager(Manager):
         self._conn_mgr.send_breakpoint_remove(id, callback)
 
     def on_io_event(self, source, condition):
+        # Called from the gobject main loop when an event occurs on the 
+        # connection socket.
         if condition == gobject.IO_IN:
             self._conn_mgr.process_response()
         elif condition == gobject.IO_HUP:
@@ -91,6 +93,8 @@ class PageManager(Manager):
             self._page_num = page_num
 
     def on_button_press_on_source_view(self, source_view, ev):
+        # Called by gtk when a button press has occurred on the sourceview
+        # control. This method sets or removes line breakpoints.
         if not self._conn_mgr.can_interact:
             return
         # ensure that click occurred on the left gutter
@@ -103,18 +107,27 @@ class PageManager(Manager):
                 self._remove_line_breakpoint(iter.get_line())
 
     def on_command(self, mgr, command):
+        # Called by conn_mgr when a command is being sent. The command string
+        # is logged to the communcations log.
         self._view['log'].log('>>', str(command))
 
     def on_response(self, mgr, response):
+        # Called by conn_mgr when a response was received. The XML is logged
+        # to the communications log.
         self._view['log'].log('<<', str(response))
 
     def on_stream_response(self, mgr, response):
+        # Called by conn_mgr when a packet of stream data is received. The data
+        # is logged to the appropriate io stream widget.
         if response.type == 'stdout':
             self._view['stdout'].log_no_prefix(response.data)
         elif response.type == 'stderr':
             self._view['stderr'].log_no_prefix(response.data)
 
     def on_init_packet(self, mgr, init, remote_addr):
+        # Called by conn_mgr when the initial XML packet is received from the
+        # engine. Appends a new notebook page and requests the source for the
+        # initial script.
         conn_info = init.get_engine_info()
         addr_info = { 'remote_ip': remote_addr[0], \
             'remote_port': remote_addr[1] }
@@ -133,22 +146,32 @@ class PageManager(Manager):
         mgr.send_source(file_uri, callback)
 
     def on_init_source(self, mgr, response, init_file_uri):
+        # Called by conn_mgr after on_init_packet when the source of the script
+        # file being debugged is received. The source is cached and the 
+        # sourceview is updated with the text.
         source = Source(init_file_uri, response.source)
         self._source_cache[init_file_uri] = source
         self._view['source_view'].current_source = source
         mgr.send_stdout(observer=self.on_init_stdout)
 
     def on_init_stdout(self, mgr, response):
+        # Called by conn_mgr after on_init_source with the result of the stdout
+        # command.
         mgr.send_stderr(observer=self.on_init_stderr)
 
     def on_init_stderr(self, mgr, response):
-        # stderr is a core command, yet xdebug does not support it :) so we
-        # remove the tab page if the command fails. (this is true as of v2.0.3)
+        # Called by conn_mgr after on_init_stdout with the result of the stderr
+        # command. stderr is a core command, yet xdebug does not support it :) 
+        # so we remove the tab page if the command fails. (this is true as of 
+        # v2.0.3)
         if not response.successful:
             self._view.remove_stderr_page()
         mgr.send_status(self.on_init_status)
 
     def on_init_status(self, mgr, response):
+        # Called by conn_mgr after on_init_stderr with the initial status of
+        # the debugger engine.  Makes the new tab page show up.
+
         # The page's initial state is prepared, make it show up.
         self._view['outer_box'].show_all()
 
@@ -157,27 +180,40 @@ class PageManager(Manager):
         app_view['notebook'].set_current_page(self._page_num)
 
     def on_cont_status(self, mgr, response):
+        # Called by conn_mgr when a status response is received resulting from
+        # a continuation command.  Requests stack information if the engine
+        # is stopped on a line. Or, if not, updates the display accordingly.
         if response.status == 'break':
             mgr.send_stack_get(self.on_cont_stack_get)
         else:
             self._view['source_view'].unset_current_line()
 
     def on_cont_stack_get(self, mgr, response):
-        # check the source file in the source view currently, and
-        # change it if it differs from the top of the stack.
+        # Called by conn_mgr after on_cont_status, with a stack_get response.
+        # Updates the the source in the source view and the line number if 
+        # the file on the top of the stack is in the source cache. Or, requests
+        # the source file on the top of the stack.
         stack = response.get_stack_elements()
         top = stack[0]
-        
+        source_view = self._view['source_view']
+
+        # check if the sourceview is showing the file on the top of the stack.
+        # If so, update the current line and return.
+        if source_view.current_source.file_uri == top['filename']:
+            source_view.set_current_line(top['lineno'] - 1)
+            return
+
         if self._source_cache.has_key(top['filename']):
-            self._view['source_view'].current_source = \
-                self._source_cache[top['filename']]
-            self._view['source_view'].set_current_line(top['lineno'] - 1)
+            source_view.current_source = self._source_cache[top['filename']]
+            source_view.set_current_line(top['lineno'] - 1)
         else:
             file_uri = top['filename']
             callback = bind_params(self.on_source_update_view, stack)
             mgr.send_source(file_uri, callback)
 
     def on_source_update_view(self, mgr, response, last_stack):
+        # Called by conn_mgr after on_cont_stack_get, with a source response.
+        # Updates the source in the sourceview and the current line number.
         top = last_stack[0]
         source = Source(top['filename'], response.source)
         self._source_cache[top['filename']] = source
@@ -185,6 +221,8 @@ class PageManager(Manager):
         self._view['source_view'].set_current_line(top['lineno'] - 1)
     
     def on_breakpoint_set(self, mgr, response, source, line_num):
+        # Called by conn_mgr with a breakpoint_set response. If the command
+        # was successful, updates the internal state and the display.
         if isinstance(response, BreakpointSetResponse):
             source.add_breakpoint(line_num, response.id, 'line')
             self._view['source_view'].refresh_breakpoints()
@@ -193,6 +231,8 @@ class PageManager(Manager):
             pass
 
     def on_breakpoint_remove(self, mgr, response, source, id):
+        # Called by conn_mgr with a breakpoint_remove response. If the command
+        # was successful, updates the internal state and the display.
         if isinstance(response, BreakpointRemoveResponse):
             source.remove_breakpoint(id)
             self._view['source_view'].refresh_breakpoints()
